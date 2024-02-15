b1cb4d60541551051bf501c4ec7a0c81ec6c7629
marc <marc.espinosa-serra.external@capgemini.com>
2024-02-15 10:57:31 +0100

    Inheritance3 - Overloading and Overriding

---
+++ java/InheritanceExo3.java
@@ -0,0 +1,35 @@
+public class InheritanceExo3 {
+
+    public class Shape {
+
+        //Original get Area method
+        public double getArea(){
+            return 0;
+        }
+        //Overload ("expantion") of Area method
+        public double getArea(int area){
+            return area;
+        }
+        //Overload ("expantion") of Area method
+        public double getArea(double area){
+            return area;
+        }
+
+    }
+
+    public class Rectangle extends Shape {
+        private double length;
+        private double width;
+
+        public Rectangle(double length, double width){
+            this.length = length;
+            this.width = width;
+        }
+        
+        //Override of Area method
+        @Override
+        public double getArea(){
+            return length * width;
+        }
+    }
+}
--- java/Main.java
+++ java/Main.java
@@ -8,10 +8,22 @@ public class Main {
 
         //Notes notes = new Notes();
         //notes.MyNotes();
+
+        //Only possible because MyNotes is STATIC
         //Notes.MyNotes();
 
         InheritanceExo1.Animal.makeSound();
         InheritanceExo1.Cat.makeSound();
+
+        //CALLING an "inner non-static class" :
+        InheritanceExo3 obj = new InheritanceExo3();
+        InheritanceExo3.Shape shape = obj.new Shape();
+        
+        InheritanceExo3.Rectangle rectangle = obj.new Rectangle(3, 3);
+
+        System.out.println(shape.getArea(9));
+        System.out.println(shape.getArea());
+        System.out.println(rectangle.getArea());
 
         
 
